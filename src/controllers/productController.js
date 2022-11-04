dc8d48fcf2053c257a6ed12376b6aa1ede28a770
SiddhantYerandkar <siddhantyerandkar@gmail.com>
2022-11-04 12:51:22 +0530

    done

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -21,12 +21,42 @@ const createOrder = async function (req, res){
     let idValue = data.userId
     let productValue = data.productId
 
-    if(!idValue) return res.send({msg: 'userId is mandatory'})
-    if(!productValue) return res.send({msg: 'productId is mandatory' })
+    let productId = await productModel.findById(productValue)
+    let userId = await userDocumentModel.findById(idValue)
 
-    let savedData = await orderModel.create(data)
+    if(!idValue) return res.send({msg: "userId is mandatory"})
+    if(!userId) return res.send({msg : "enter valid userId" })
 
-    res.send(savedData)
+    if(!productValue) return res.send({msg: "productId is mandatory" })
+    if(!productId) return res.send({msg: "enter valid productId"})
+
+    let isFreeApp = req.headers['isfreeappuser']
+    
+    let productPrice = productId.price
+
+    let userBalance = userId.balance
+
+    if(isFreeApp == "true"){
+        data["amount"] = 0
+        let saved = await orderModel.create(data)
+        return res.send({msg: saved})
+    }else{
+        if( userBalance > productPrice ){
+            let updatedBalance = userBalance - productPrice
+            let newbalance = await userDocumentModel.findOneAndUpdate(
+                {_id: userId },
+                { $set: { balance: updatedBalance} }
+            )
+            
+            let savedData = await orderModel.create(data)
+            return res.send(savedData)
+
+        } else{
+            return res.send({msg: "user dont have enough balance"})
+        }
+
+    }
+
 }
 
 
--- src/middlewares/validationMiddleware.js
+++ src/middlewares/validationMiddleware.js
@@ -2,9 +2,14 @@ const theMidMiddleware = function (req, res, next ){
     let acceptHeader = req.headers['isfreeappuser']
     
     if(acceptHeader) {
+        if(req.headers['isfreeappuser']== "true"){
+            req.body.isFreeAppUser = true
+        }else{
+            req.body.isFreeAppUser = false
+        }
         next()
     } else {
-        return res.send("error")
+        return res.send("error: the request is missing the mandatory header")
     }
     
 }
